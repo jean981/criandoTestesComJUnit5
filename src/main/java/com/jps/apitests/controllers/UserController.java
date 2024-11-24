3fbfbe6571bb1f07c198b1fdd2fb5e9bf362c21a
jean981 <jeanpdesa@gmail.com>
2024-11-24 01:06:14 +0000

    "✨ feat(UserController): added method findById"

--- src/main/java/com/jps/apitests/controllers/UserController.java
+++ src/main/java/com/jps/apitests/controllers/UserController.java
@@ -2,6 +2,8 @@ package com.jps.apitests.controllers;
 
 
 import com.jps.apitests.domain.UUser;
+import com.jps.apitests.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,11 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/user")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<UUser> findById(@PathVariable Integer id) {
 
         return
-              ResponseEntity.ok().body(new UUser(1,"bob fisher","bob@gamil.com","senha123"));
+              ResponseEntity.ok().body(userService.findById(id));
 
     }
 
