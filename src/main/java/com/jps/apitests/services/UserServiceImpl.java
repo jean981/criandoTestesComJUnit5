e8cac17da014ca21dbe6285ae4e3a6da85a33610
jean981 <jeanpdesa@gmail.com>
2024-11-24 01:01:01 +0000

    🚏 services entity: user service created"

---
+++ src/main/java/com/jps/apitests/services/UserServiceImpl.java
@@ -0,0 +1,23 @@
+package com.jps.apitests.services;
+
+import com.jps.apitests.domain.UUser;
+import com.jps.apitests.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UUser findById(Integer id) {
+
+        Optional<UUser> user = userRepository.findById(id);
+
+        return  user.orElse(null);
+    }
+}
