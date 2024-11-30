ffcb092eb8514e2b2d03c70f3148ddf0cf103609
jean981 <jeanpdesa@gmail.com>
2024-11-30 03:31:54 +0000

    🎚 config(start_db): created list of users in the in-memory database.

--- src/main/java/com/jps/apitests/config/LocalConfig.java
+++ src/main/java/com/jps/apitests/config/LocalConfig.java
@@ -19,9 +19,12 @@ public class LocalConfig {
     public void startDb(){
 
         UUser u1 = new UUser(null,"bob fisher","bob@gmail.com","pass123");
-        UUser u2 = new UUser(null,"maria louca","louca@gmail.com","pass123");
+        UUser u2 = new UUser(null,"maria louca","louca@gmail.com","pass345");
+        UUser u3 = new UUser(null,"mario bros","mario@gmail.com","pass567");
+        UUser u4 = new UUser(null,"paul caugton","paulcaugton@gmail.com","pass789");
+        UUser u5 = new UUser(null,"sergi silva","sergiosilva@gmail.com","pass098");
 
-        userRepository.saveAll(List.of( u1,u2));
+        userRepository.saveAll(List.of( u1,u2,u3,u4,u5));
 
     }
 }
